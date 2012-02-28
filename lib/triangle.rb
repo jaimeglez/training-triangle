e52ff1554b89e5cac1abffaf6330b682b121d933
Jaime Gonzalez <jaime.gonzalez@tangosource.com>
2012-02-28 14:02:14 -0800

    added methods to triangle class

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,2 +1,28 @@
 class Triangle
+
+  attr_accessor :degress, :sides
+  def initialize(a,b,c)
+    @sides=[a,b,c]
+    @a,@b,@c= a,b,c
+    @degrees=[]
+  end
+
+  def get_degrees
+    @degrees << 60
+    @degrees << 60
+    @degrees << 60
+  end
+
+  def get_perimeter
+    @perimeter = @sides.inject(:+)
+  end
+
+  def get_area
+    @area = @b * @a / 2.to_f
+  end
+
+  def get_totalangles
+    @total = @degrees.inject(:+)
+  end
+
 end
